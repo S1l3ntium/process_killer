49b1df1e9c35d33987535b294b4468dd08b10015
Denis <35502557+S1l3ntium@users.noreply.github.com>
2020-11-02 14:57:49 +0300

    Add files via upload

---
+++ Practice/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Exit = new System.Windows.Forms.Button();
+            this.About = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Kill = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.Button();
+            this.Find = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(713, 347);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 20);
+            this.Exit.TabIndex = 0;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // About
+            // 
+            this.About.Location = new System.Drawing.Point(713, 321);
+            this.About.Name = "About";
+            this.About.Size = new System.Drawing.Size(75, 20);
+            this.About.TabIndex = 1;
+            this.About.Text = "About";
+            this.About.UseVisualStyleBackColor = true;
+            this.About.Click += new System.EventHandler(this.About_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(695, 394);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // Kill
+            // 
+            this.Kill.Location = new System.Drawing.Point(713, 12);
+            this.Kill.Name = "Kill";
+            this.Kill.Size = new System.Drawing.Size(75, 20);
+            this.Kill.TabIndex = 5;
+            this.Kill.Text = "Kill";
+            this.Kill.UseVisualStyleBackColor = true;
+            this.Kill.Click += new System.EventHandler(this.Kill_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox1.Location = new System.Drawing.Point(12, 418);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(695, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(713, 67);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 20);
+            this.Clear.TabIndex = 7;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(713, 38);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 23);
+            this.Search.TabIndex = 8;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // Find
+            // 
+            this.Find.Location = new System.Drawing.Point(713, 418);
+            this.Find.Name = "Find";
+            this.Find.Size = new System.Drawing.Size(75, 20);
+            this.Find.TabIndex = 9;
+            this.Find.Text = "Find";
+            this.Find.UseVisualStyleBackColor = true;
+            this.Find.Click += new System.EventHandler(this.Find_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(796, 450);
+            this.Controls.Add(this.Find);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Kill);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.About);
+            this.Controls.Add(this.Exit);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Process Killer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button About;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Kill;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Button Find;
+    }
+}
+
---
+++ Practice/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Diagnostics;
+using System.Management;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void About_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Process Killer v3.1.2");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process[] processes;
+            processes = System.Diagnostics.Process.GetProcesses();
+            foreach (System.Diagnostics.Process instance in processes)
+            {
+                listBox1.Items.Add(instance.ProcessName);
+            }
+        }
+
+        private void Kill_Click(object sender, EventArgs e)
+        {
+            foreach (var process in Process.GetProcessesByName(textBox1.Text))
+            {
+                process.Kill();
+            }
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                textBox1.Text = listBox1.SelectedItem.ToString();
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process[] processes;
+            processes = System.Diagnostics.Process.GetProcesses();
+            foreach (System.Diagnostics.Process instance in processes)
+            {
+                listBox1.Items.Add(instance.ProcessName);
+            }
+        }
+
+        private void Find_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                int index = listBox1.FindString(textBox1.Text);
+                if (index != -1)
+                    listBox1.SetSelected(index, true);
+                else
+                    MessageBox.Show("This process was not found among the running ones.");
+            }
+        }
+    }
+}
